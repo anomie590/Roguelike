f5a97f64d4a0716e289660924c82d6a5d8f3f074
Matthew Le <maattt01@gmail.com>
2020-09-13 22:05:54 -0500

    added generic Entity, render functions, and map

--- engine.py
+++ engine.py
@@ -8,8 +8,13 @@ from render_functions import render_all, clear_all
 def main():
     screen_width = 80
     screen_height = 50
+
     map_width = 80
     map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
@@ -17,7 +22,7 @@ def main():
     }
 
     player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
-    npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', libtcod.yellow)
+    npc = Entity(int(screen_width/2 - 4), int(screen_height/2), '@', libtcod.yellow)
     entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -27,6 +32,7 @@ def main():
     con = libtcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
